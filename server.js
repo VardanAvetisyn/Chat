c8389dadbb547e7c5fc675f5c2614b16c21cd4a6
VardanAvetisayn <vardan.avetisyan1@tumo.org>
2024-04-08 19:22:24 +0400

    first push

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require('path');
+const http = require('http');
+const socket = require('socket.io');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const server = http.createServer(app);
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('User Connected Successful!');
+    socket.emit("message","Wellcome to server")
+    socket.broadcast.emit("message","A new user joind")
+    socket.on('chatMsg',(m) => {
+      io.emit("message",m)
+    })
+    socket.on('disconnect',() => {
+      io.emit("message","A user has left")
+    })
+});
+
+server.listen(PORT, () => {
+console.log(`server is running on port ${PORT}`);
+});
